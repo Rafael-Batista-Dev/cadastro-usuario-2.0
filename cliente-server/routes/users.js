b65c7a75fc0cc408ddacd68c1f2c69c9fca8f9f0
Rafael-Batista-Dev <59888617+Rafael-Batista-Dev@users.noreply.github.com>
2020-03-23 09:09:51 -0300

    Consumindo Rotas, POST, PUT e DELETE com Restify

--- cliente-server/routes/users.js
+++ cliente-server/routes/users.js
@@ -17,13 +17,43 @@ router.get("/", function(req, res, next) {
   });
 });
 
-/* GET users listing. */
-router.get("/", function(req, res, next) {
-  client.get("/users", function(err, request, response, obj) {
+/* GET users listing. at ID*/
+router.get("/:id", function(req, res, next) {
+  client.get(`/users/${req.params.id}`, function(err, request, response, obj) {
     assert.ifError(err);
 
     res.end(JSON.stringify(obj, null, 2));
   });
 });
 
+/* Updete */
+router.put("/:id", function(req, res, next) {
+  client.put(`/users/${req.params.id}`, req.body, function(
+    err,
+    request,
+    response,
+    obj
+  ) {
+    assert.ifError(err);
+
+    res.end(JSON.stringify(obj, null, 2));
+  });
+});
+
+/*Delete*/
+router.delete("/:id", function(req, res, next) {
+  client.del(`/users/${req.params.id}`, function(err, request, response, obj) {
+    assert.ifError(err);
+
+    res.end(JSON.stringify(obj, null, 2));
+  });
+});
+
+router.post("/:id", function(req, res, next) {
+  client.post(`/users/`, req.body, function(err, request, response, obj) {
+    assert.ifError(err);
+
+    res.end(JSON.stringify(obj, null, 2));
+  });
+});
 module.exports = router;
