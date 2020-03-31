c8c37500fde2397045ad94090903e7596c8b54ab
unknown <rafaelbatista.sth@gmail.com>
2020-03-31 11:58:04 -0300

    Método delete

--- cliente-server/public/controllers/UserController.js
+++ cliente-server/public/controllers/UserController.js
@@ -155,7 +155,7 @@ class UserController {
   }
 
   selectAll() {
-    HttpRequest.get("./users").then(data => {
+    User.getUsersStorage("./users").then(data => {
       data.users.forEach(dataUser => {
         let user = new User();
         user.loadFromJSON(dataUser);
@@ -198,9 +198,10 @@ class UserController {
       if (confirm("Deseja realmente excluir")) {
         let user = new User();
         user.loadFromJSON(JSON.parse(tr.dataset.user));
-        user.remove();
-        tr.remove();
-        this.updateCount();
+        user.remove().then(data => {
+          tr.remove();
+          this.updateCount();
+        });
       }
     });
 
